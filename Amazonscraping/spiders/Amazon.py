265921f80269280ec573dcdfd3e3d1cc7bd2293e
ilham mohammad <omdilham111@gmail.com>
2022-01-10 00:02:18 +0530

    First commit

---
+++ Amazonscraping/spiders/Amazon.py
@@ -0,0 +1,19 @@
+import scrapy
+
+
+class AmazonSpider(scrapy.Spider):
+    name = 'Amazon'
+    start_urls = ['https://www.amazon.in/s?k=laptops']
+
+    def parse(self, response):
+        for products in response.css('.s-result-item'):
+            yield {
+                'Product_name': products.css('span.a-size-medium.a-color-base.a-text-normal::text').get(),
+                'Product_Link': products.css('a.a-link-normal.a-text-normal::attr(href)').get(),
+                'Rating': products.css('span.a-icon-alt::text').get(),
+                'Price': products.css('span.a-price-whole::text').get()
+                }
+        next_page = response.css('li.a-last > a::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
+
